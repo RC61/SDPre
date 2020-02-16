c2e319c6df27689e3841c0fc647864275e56c129
RC61 <jetwestpack@gmail.com>
2020-02-16 07:49:47 -0500

    O2L2 created

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Rory Coleman";
+    int age = 28;
+    String state = "Florida";
+
+    System.out.println( name );
+    System.out.println( age );
+    System.out.println( state );
+  }
+}
