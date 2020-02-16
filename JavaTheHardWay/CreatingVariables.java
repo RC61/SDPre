d91cf8820f01e1e50b8f5a9d24fdeb6e96f3b1e9
Rory <51101973+RC61@users.noreply.github.com>
2020-02-16 11:17:28 -0500

    Update CreatingVariables.java

--- JavaTheHardWay/CreatingVariables.java
+++ JavaTheHardWay/CreatingVariables.java
@@ -7,18 +7,18 @@ public class CreatingVariables {
 		x = 10;
 		y = 400;
 		age = 39;
-    extraInt = 1;
+    		extraInt = 1;
 
 		seconds = 4.71;
 		e = 2.7182818184523536;
 		checking = 1.89;
-    extraDouble = 1.12;
+		extraDouble = 1.12;
 
 		firstName = "Graham";
 		lastName = "Mitchell";
 		title = "Mr.";
-    extraString1 = "Ello";
-    extraString2 = "Gov'na";
+    		extraString1 = "Ello";
+   		extraString2 = "Gov'na";
 
 		System.out.println( "The variable x contains " + x );
 		System.out.println( "The value " + y + " is stored in the variable y." );
