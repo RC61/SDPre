d8c7b798b4b7c484ccf2901eabcf7e6f0018b49f
RC61 <jetwestpack@gmail.com>
2020-02-20 06:04:34 -0500

    CN created

--- JavaTheHardWay/ComparingNumbers.java
+++ JavaTheHardWay/ComparingNumbers.java
@@ -4,12 +4,12 @@ public class ComparingNumbers {
 	public static void main( String[] args ) {
 		Scanner keyboard = new Scanner(System.in);
 		double first, second;
-		
+
 		System.out.print( "Give me two numbers. First: " );
 		first = keyboard.nextDouble();
 		System.out.print( "Second: " );
 		second = keyboard.nextDouble();
-		
+
 		if ( first < second ) {
 			System.out.println( first + " is LESS THAN " + second );
 		}
@@ -20,14 +20,14 @@ public class ComparingNumbers {
 			System.out.println( first + " is EQUAL TO " + second );
 		}
 		if ( first >= second ) {
-			System.out.println( first + " is GREATER THAN/EQUAL TO " );
+			System.out.println( first + " is GREATER THAN/EQUAL TO " + second);
 		}
 		if (first > second ) {
-			System.out.println( first + " is GREATER THAN " );
+			System.out.println( first + " is GREATER THAN " + second);
 		}
 		if (first != second )
-			System.out.println( first + "is NOT EQUAL TO " );
-			
+			System.out.println( first + "is NOT EQUAL TO " + second);
+
 			// complete the study drills please
 	}
 }
