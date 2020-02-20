c916e014304257c9edb72d759132a18f3be16d7e
RC61 <jetwestpack@gmail.com>
2020-02-20 06:16:55 -0500

    SW edited

--- JavaTheHardWay/SecretWord.java
+++ JavaTheHardWay/SecretWord.java
@@ -1,15 +1,15 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 public class SecretWord {
 	public static void main( String[] args ) {
 		Scanner keyboard = new Scanner(System.in);
 		String secret = "please", guess;
-		
+
 		System.out.print( "What's the secret word? " );
 		guess = keyboard.next();
-		
-		if ( guess.equals(secret) || guess.equals("shenanigans") {
-			Systems.out.println( "That's correct!" );
+
+		if ( guess.equals(secret) || guess.equals("shenanigans") ) {
+			System.out.println( "That's correct!" );
 // wanted to declare and initialize new variable, just plugged in a string instead.
 		}
 		else {
@@ -17,6 +17,6 @@ public class SecretWord {
 		}
 		if (guess == secret ) {
 			System.out.println( "(This will never print, no matter what.)" );
-		}                                                                                                                 
+		}
 	}
 }
