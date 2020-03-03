f3ff0ca25fd11c9c505c85a77b123216475dc40d
RC61 <jetwestpack@gmail.com>
2020-03-03 10:33:59 -0500

    create O6L4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,14 @@
+public class Objective6Lab4 {
+  public static void main(String[] args ){
+    int counter, sumTotal;
+
+    counter = 0 ;
+    sumTotal = 0 ;
+
+    while ( counter <= 19 ){
+      counter += 1;
+      sumTotal += counter;
+    }
+    System.out.println( sumTotal );
+  }
+}
